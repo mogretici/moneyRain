31890d265167670b9eebf72129f5284818e5abc9
Lütfü ÖĞRETİCİ <mogretici@yandex.com.tr>
2024-09-12 12:40:06 +0300

    arView

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ const App: React.FC = () => {
                     max-camera-orbit="auto 90deg auto"
                     ar-modes="webxr scene-viewer quick-look"
                     scale="0.2 0.2 0.2"
+                    animation-name="Car"
                     shadow-intensity="1"
                     ar-placement="floor"
                     auto-rotate="true"
